5add5f3a6a1e4f028b52200133681c908d6ad10c
prasad sripathi <prasad18218@gmail.com>
2025-09-27 23:12:40 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -3,13 +3,12 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-00f51e3430243ff58"
 ZONE_ID="Z015504635YTPJNLLKES7"
-DOM
 DOMAIN_NAME="spdevhops.shop"
 for instance in $@
 do
      INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00f51e3430243ff58 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-     if [ $instance != "frontend"]; then
+     if [ $instance != "frontend" ]; then
      IP=$(aws ec2 describe-instances --instance-ids $InstanceId --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
      RECORD_NAME="$instance.$DOMAIN_NAME"
     else
