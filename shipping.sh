21c8530d56dacaeb60dbe8b6b8beb2e7332a03a3
prasad sripathi <prasad18218@gmail.com>
2025-10-05 09:11:18 +0530

    Roboshop project

--- shipping.sh
+++ shipping.sh
@@ -32,48 +32,44 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 
 dnf install maven -y &>>$LOG_FILE
 
-id roboshop &>>$LOG_FILE
+id roboshop
 if [ $? -ne 0 ]; then
    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
    VALIDATE $? "Creating system user"
 else
-   echo -e "User already exist"...$Y Skipping $N"
+   echo -e "User already exist ... $Y SKIPPING $N"
 fi
-
-mkdir -p /app
+mkdir -p /app &>>$LOG_FILE
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading shipping application"
 
 cd /app
+VALIDATE $? "Chaning to app directory"
 
 rm -rf /app/*
 VALIDATE $? "Removing existing code"
 
-unzip /tmp/shipping.zip &>>$LOG_FILE
-validate $? "unzip shipping"
+unzip /tmp/shipping.zip
+VALIDATE $? "unzip shipping"
 
 mvn clean package &>>$LOG_FILE
+mv target/shipping-1.0.jar shipping.jar
 
-mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
-
-cp $SCRIPT_DIR/shipping.service  /etc/systemd/system/shipping.service
-
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOG_FILE
 systemctl daemon-reload
-
 systemctl enable shipping &>>$LOG_FILE
 
-dnf install mysql -y &>>$LOG_FILE
+dnf install mysql -y
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
-
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
 if [ $? -ne 0 ]; then
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
 else
-    echo "Shipping data is already loaded"
+   echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
 fi
 
 systemctl restart shipping
